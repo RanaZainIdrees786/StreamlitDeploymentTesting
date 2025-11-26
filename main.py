30d4f85af6a8b448312b2b3995dbba8050e07e13
RanaZainPNY <rzi.codealigned@gmail.com>
2025-11-26 14:23:00 +0500

    changed user input

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.title("Hello Welcome to Streamlit")
 st.write('this is a demo app to test streamlit deployment')
 userinput = st.text_input("Hi how are you")
 
-st.write(userinput)
-st.write("Thank you for your response")
-st.write("Have a great day")
+if userinput:
+    st.write(userinput)
+    st.write("Thank you for your response")
+    st.write("Have a great day")
