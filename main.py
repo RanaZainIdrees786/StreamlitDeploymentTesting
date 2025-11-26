9e81eef1103a850cdb10228fed63ac3700aaba3e
RanaZainPNY <rzi.codealigned@gmail.com>
2025-11-26 14:08:32 +0500

    changed input to input_text

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title("Hello Welcome to Streamlit")
 st.write('this is a demo app to test streamlit deployment')
-userinput=st.input("Hi how are you")
+userinput = st.input_text("Hi how are you")
+
+st.write(userinput)
+st.write("Thank you for your response")
+st.write("Have a great day")
