1f83e7e178a30742592fa97f647e925a56c25d7e
RanaZainPNY <rzi.codealigned@gmail.com>
2025-11-26 14:10:30 +0500

    modified text_input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Hello Welcome to Streamlit")
 st.write('this is a demo app to test streamlit deployment')
-userinput = st.input_text("Hi how are you")
+userinput = st.text_input("Hi how are you")
 
 st.write(userinput)
 st.write("Thank you for your response")
